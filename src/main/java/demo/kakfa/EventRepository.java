1d9b45389caaf7ccedceee2dc3a8b1e2a65e781f
Jyotiswarup Raiturkar <Jyotiswarup.Pairaiturkar@walmartlabs.com>
2020-03-18 17:19:02 +0530

    comment for why save() was not used

--- src/main/java/demo/kakfa/EventRepository.java
+++ src/main/java/demo/kakfa/EventRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<EventEntity,String> {
+    // Unfortunately JPA does not have a concept of insert.
+    // save() will do an upsert and not throw any exception - hence this hack for now
     @Modifying
     @Query(value = "INSERT INTO EVENTS (ID,NAME,TOTAL_SALE_PRICE) VALUES (:id, :name, :price)",nativeQuery = true)
     public void insertEvent(@Param("id") String id, @Param("name")String name, @Param("price") Long totalPrice);
